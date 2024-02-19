370ebc59344c3e9d73a2f1f85e4f82f61b0a13fe
Jay Teramoto <jayteramoto@gmail.com>
2024-02-19 22:12:13 +0900

    update Time UTC->JST, CSV format

--- GPS_LoRa_tracker_Receiver/GPS_LoRa_tracker_Receiver.ino
+++ GPS_LoRa_tracker_Receiver/GPS_LoRa_tracker_Receiver.ino
@@ -3,8 +3,8 @@ GPS_LoRa_tracker_Receiver.ino
 Receiver application (Receive message(GPSdata) via LoRa and display on OLED, and share on WiFi)
 Platform: Wemos D1 mini ESP32
 
-For use of ESP32, use modified LoRa_E220.h (2024.2.3)
-
+For use of espSoftwareSerial on ESP32, use modified LoRa_E220.h (2024.2.3)
+*** LoRa *** EBYTE E220-900T22D
  * E220       ----- Wemos D1 mini     ----- Wemos D1 MINI ESP32(compatible pins)
  * M0         ----- D5                ----- D5
  * M1         ----- D6                ----- D6
@@ -14,13 +14,26 @@ For use of ESP32, use modified LoRa_E220.h (2024.2.3)
  * VCC        ----- 3.3v              ----- 3.3V
  * GND        ----- GND               ----- GND
 
- * GPS       ----- Wemos D1 mini
+*** OLED *** SH1106
+ * SH1106    ----- Wemos D1 mini     ----- Wemos D1 MINI ESP32(compatible pins)
+ * VCC        ----- 3.3v              ----- 3.3V
+ * GND        ----- GND               ----- GND
+ * SCL        ----- Dx                ----- Dx
+ * SDA        ----- Dx                ----- Dx
+
+*** GPS ***
+ * GPS        ----- Wemos D1 mini     ----- Wemos D1 MINI ESP32(compatible pins)
  * RX         ----- D4 (No pull-up)   ----- D4
  * TX         ----- D0 (No pull-up)   ----- D0
- * VCC        ----- 3.3v
- * GND        ----- GND
+ * VCC        ----- 3.3v              ----- 3.3v
+ * GND        ----- GND               ----- GND
 
-2024.02.10  Rev.0.1
+2024.02.10  Rev.0.1 Initial release
+2024.02.15  Rev.0.2 Change OLED device from SSD1306 to SH1106
+2024.02.19  Rev.0.3 TimeLib to UTC to JST, DEBUG output changed for CSV format
+
+TODO: WiFi 
+
 Author : Jay Teramoto
 https://github.com/lovefool/GPS_LoRa_tracker/tree/main
 ***************************************************/
@@ -29,7 +42,8 @@ https://github.com/lovefool/GPS_LoRa_tracker/tree/main
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h> //https://github.com/adafruit/Adafruit-GFX-Library
-#include <Adafruit_SSD1306.h> //https://github.com/adafruit/Adafruit_SSD1306
+// #include <Adafruit_SSD1306.h> //https://github.com/adafruit/Adafruit_SSD1306
+#include <Adafruit_SH110X.h> //https://github.com/adafruit/Adafruit_SH110X
 #include <TimeLib.h> //https://playground.arduino.cc/Code/Time/
 
 #define GPS_LoRa_DEBUG
@@ -59,7 +73,8 @@ LoRa_E220 e220ttl(&LoraSer, D3, D5, D6); // AUX M0 M1
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SH1106G oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 struct LoRamessage {  // message structure sent to receiver
   char    id[9];
@@ -75,6 +90,8 @@ struct LoRamessage {  // message structure sent to receiver
 }; 
 struct LoRamessage msg = {"        ", 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+#define time_offset 32400    // UTC+9時間(3600 * 9 秒）
+
 void setup() {
   delay(500);
   DBG_PRINTER.begin(9600);
@@ -82,18 +99,24 @@ void setup() {
   delay(500);
   
   e220ttl.begin();                        // Start LoRa E220}
-  oled.begin(SSD1306_SWITCHCAPVCC, 0x3c); // Start OELD SSD1306
+//  oled.begin(SSD1306_SWITCHCAPVCC, 0x3c); // Start OELD SSD1306
+  oled.begin(SCREEN_ADDRESS, true); // Start OELD SH1106
 
   // Display initial 
   oled.clearDisplay();
   oled.setTextSize(2);             
-  oled.setTextColor(WHITE);
+//  oled.setTextColor(WHITE); // SSD1306
+  oled.setTextColor(SH110X_WHITE); //SH1106
 
   oled.setCursor(0,0);      
-  oled.print("    GPS-LoRa");
+  oled.print(" GPS-LoRa");
 
   oled.setCursor(0,18);
-  oled.print("Tracker0.1");
+  oled.print("  Tracker");
+
+  oled.setCursor(0,36);
+  oled.print("   v0.1");
+
 
   oled.display();
 
@@ -120,21 +143,24 @@ void loop() {
 
       msg = *(LoRamessage*) rsc.data;
 
-      DBG_PRINT(msg.id);    // Structured data
-      DBG_PRINT(" "); 
-      sprintf(sz, "%6d",msg.count); DBG_PRINTLN(msg.count); 
-      sprintf(sz, "%10.6f",msg.gpslat); DBG_PRINTLN(sz);
-      sprintf(sz, "%10.6f",msg.gpslng); DBG_PRINTLN(sz);
-      sprintf(sz, "%04d/%02d/%02d %02d:%02d:%02d", msg.gpsyear, msg.gpsmonth, 
-              msg.gpsday, msg.gpshour, msg.gpsminute, msg.gpssecond);
-      DBG_PRINTLN(sz);
+      // Serial output of Structured data (msg)
+      DBG_PRINT(msg.id);DBG_PRINT(", "); //id
+      sprintf(sz, "%6d",msg.count); DBG_PRINT(msg.count);DBG_PRINT(", "); // count
+      sprintf(sz, "%10.6f",msg.gpslat); DBG_PRINT(sz);DBG_PRINT(", "); // latitude
+      sprintf(sz, "%10.6f",msg.gpslng); DBG_PRINT(sz);DBG_PRINT(", "); // longtitude
+
+      setTime(msg.gpshour, msg.gpsminute, msg.gpssecond, msg.gpsday, msg.gpsmonth, msg.gpsyear);
+      adjustTime(time_offset);   //JST変換
+
+      sprintf(sz, "%04d/%02d/%02d %02d:%02d:%02d, ", year(),month(),day(),hour(),minute(),second());DBG_PRINT(sz); //time stamp
       
-      DBG_PRINT("RSSI: "); DBG_PRINTLN(rsc.rssi, DEC); // RSSI
+      DBG_PRINT("RSSI:"); DBG_PRINTLN(rsc.rssi, DEC); // RSSI
 
       // Display
       oled.clearDisplay();
-      oled.setTextSize(2);             
-      oled.setTextColor(WHITE);
+      oled.setTextSize(2);
+      //  oled.setTextColor(WHITE); // SSD1306
+      oled.setTextColor(SH110X_WHITE);  //SH1106             
 
       oled.setCursor(0,0);      
       sprintf(sz, "%10.6f", msg.gpslat);  
